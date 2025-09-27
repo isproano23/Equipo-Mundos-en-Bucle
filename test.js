41bea1bd6970aa923cf3ebc646f52fe16393efbf
Isaac <sebasproano917@gmail.com>
2025-09-26 21:03:56 -0500

    Se muestra en pantalla el saludo

--- test.js
+++ test.js
@@ -5,13 +5,16 @@ saludar = function(){
     let apellido;
     let saludo;
     let resultado;
+    let encabezadoSaludo=document.getElementById("lblResultado")
     //recuperar el valor de la caja de texto
     cmpNombre = document.getElementById("txtNombre");
     nombre = cmpNombre.value;
     cmpApellido = document.getElementById("txtApellido");
     apellido = cmpApellido.value;
-    
+  
+
     saludo=generarSaludo(nombre,apellido);
+    encabezadoSaludo.textContent=saludo
     resultado=console.log(saludo);
     return saludo;
 }
